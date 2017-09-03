e64b05b7f982792dde68bfb0aa2bca8080c26f07
dex oostveen <dex.werk@hotmail.com>
2017-09-03 14:04:06 +0200

    add route mapping

--- server.js
+++ server.js
@@ -6,6 +6,32 @@ app = express();
 
 app.use("/", express.static(__dirname + "/static"))
 
+app.get('/test', function (req, res) {
+  res.send('Hello World!')
+})
+
+//this maps http://localhost:8080/todo to the callback functions passed
+//so if you send a Post request to /todo you get a response of "Add Todo"
+//these should be called from main.js 
+//get to load the data and the others to modify it 
+app.route("/todo")
+  //CREATE
+  .post(function (req, res) {
+    res.send('Add Todo')
+  })
+  //READ
+  .get(function (req, res) {
+  res.send('show todos')
+  })
+  //UPDATE
+  .put(function (req, res) {
+    res.send('Update the Todo')
+  })
+  //DELETE
+  .delete(function (req, res) {
+  res.send('Got a DELETE request at /todo')
+  })
+
 /*app.get('/', function(req, res){
   var stream = fs.createReadStream('index.html');
   stream.pipe(res);
